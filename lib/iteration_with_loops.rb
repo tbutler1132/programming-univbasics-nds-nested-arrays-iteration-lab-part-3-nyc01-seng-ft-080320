d4dcd98eeeb4ae8d3f57a671b1ce7ae901088de1
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-22 14:54:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
    element_index = 0
    while element_index < src[row_index].count do
 
-     if src[row_index][element_index].class.string
+     if src[row_index][element_index].class == string
         string_array << src[row_index][element_index]
      end
 
